d9508754c036ee67d56558b8d03b3c9f47e6f02e
vladyslav <vladyslav.trundenko@gmail.com>
2026-05-24 15:22:54 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const menu = document.getElementById('menu');
+const menuOpen = document.getElementById('menu-open');
+const menuClose = document.getElementById('menu-close');
+const menuLinks = document.querySelectorAll('.menu__link');
+
+menuOpen.addEventListener('click', () => {
+  menu.classList.add('menu--open');
+});
+
+menuClose.addEventListener('click', () => {
+  menu.classList.remove('menu--open');
+});
+
+menuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('menu--open');
+  });
+});
